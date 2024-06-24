1ce48b4535a68f1cd51a0cb31515db6f6961004f
¨Samia <samia.camacho17@gmail.com>
2024-06-24 15:38:54 -0500

    inicialice mi proyecto mobile con angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+    path:'principal',
+    // para cargar mi pagina de manera asyncrona simpre usar este!
+    loadComponent: () => import('./principal/principal.page').then(m => m.PrincipalPage)
+  }
 ];
