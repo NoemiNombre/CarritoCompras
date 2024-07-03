3bd7382b250918bf2768816f02d9069483bc5b69
¨Samia <samia.camacho17@gmail.com>
2024-07-02 19:02:14 -0500

    si

--- src/app/cliente/cliente.page.ts
+++ src/app/cliente/cliente.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonInput, IonItem } from '@ionic/angular/standalone';
+import {IonicModule} from '@ionic/angular'
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.page.html',
   styleUrls: ['./cliente.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class ClientePage implements OnInit {
 
